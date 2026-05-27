09d37be03ba3cacd53c05616f4a8256c9fc50e3f
Sarah Bizal <sarah.bizal@gmail.com>
2026-05-27 15:45:32 -0400

    Update initial functions

---
+++ script.js
@@ -0,0 +1,11 @@
+function gameBoard() {
+  const board = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ];
+
+  function playerMovement() {}
+  function checkSymbolInCell() {}
+  function gameboardReset() {}
+}
